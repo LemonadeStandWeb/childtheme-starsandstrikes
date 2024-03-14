ed372257a0477c81837e1e50d2a80e5dfd66c975
Sergio Udave <sergio.udave@lemonadestand.org>
2024-03-14 16:02:40 -0500

    Transparent Header Template Part

--- single-locations.php
+++ single-locations.php
@@ -3,34 +3,8 @@
 Template name: Locations Single
 */ ?>
 
-<!DOCTYPE html>
-<html <?php language_attributes(); ?> class="<?php flatsome_html_classes(); ?>">
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>" />
-	<link rel="profile" href="http://gmpg.org/xfn/11" />
-	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<?php get_template_part('templates/parts/ls-cpt-header'); ?>
 
-	<?php wp_head(); ?>
-</head>
-
-<body <?php body_class(); ?>>
-
-<?php do_action( 'flatsome_after_body_open' ); ?>
-<?php wp_body_open(); ?>
-
-<a class="skip-link screen-reader-text" href="#main"><?php esc_html_e( 'Skip to content', 'flatsome' ); ?></a>
-
-<div id="wrapper">
-
-	<?php do_action( 'flatsome_before_header' ); ?>
-
-	<header id="header" class="header header transparent has-transparent <?php flatsome_header_classes(); ?>">
-		<div class="header-wrapper">
-			<?php get_template_part( 'template-parts/header/header', 'wrapper' ); ?>
-		</div>
-	</header>
-
-	<?php do_action( 'flatsome_after_header' ); ?>
         <main id="main" class="<?php flatsome_main_classes(); ?>">
 
             <?php do_action('flatsome_before_page'); ?>
