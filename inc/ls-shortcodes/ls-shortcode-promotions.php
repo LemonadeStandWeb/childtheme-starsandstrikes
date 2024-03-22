edf19ed6da9a381b9e2ead0f2d9c940859ea40ec
Sergio Udave <webteam@lemonadestand.org>
2024-03-22 11:27:14 -0500

    Updating Shortcode Styling

--- inc/ls-shortcodes/ls-shortcode-promotions.php
+++ inc/ls-shortcodes/ls-shortcode-promotions.php
@@ -11,12 +11,12 @@ function ls_shortcode_promotions()
      * Associative array to map the days of the week to a CSS class for the specials cards.
      */
     $ls_css_class_map = array(
-        'red'    => 'red-card',
+        'red'       => 'red-card',
         'maroon'    => 'maroon-card',
-        'purple'   => 'purple-card',
+        'purple'    => 'purple-card',
         'dark-blue' => 'dark-blue-card',
-        'blue'    => 'blue-card',
-        'orange'  => 'orange-card',
+        'blue'      => 'blue-card',
+        'orange'    => 'orange-card',
         'gradient'  => 'gradient-card'
     );
 
@@ -39,30 +39,27 @@ function ls_shortcode_promotions()
             $ls_promotions_image             = get_the_post_thumbnail_url();
             $ls_promotions_link              = get_the_permalink();
             $ls_promotions_title             = get_field('ls_promotions_title');
-            $ls_promotions_short_title       = get_field('ls_promotions_short_title'); 
+            $ls_promotions_short_title       = get_field('ls_promotions_short_title');
             $ls_promotions_short_description = get_field('ls_promotions_short_description');
             $ls_promotions_card_color        = get_field('ls_promotions_card_color');
 
             // Build the promotion shortcode
-            $shortcodes .= '[col span="3" span__sm="12" span__md="10" padding="0px 0px 0px 60px" padding__md="0px 0px 0px 0px"]';
-            $shortcodes .= '[row_inner]';
-            $shortcodes .= '[col_inner span__sm="12" bg_color="rgb(255,255,255)" class="special-clickable-card ' . $ls_css_class_map[$ls_promotions_card_color] . '"]';
+            $shortcodes .= '[col span="4" span__sm="12" bg_color="rgb(255,255,255)" animate="fadeInUp" class="special-clickable-card ' . $ls_css_class_map[$ls_promotions_card_color] . '"]';
             $shortcodes .= '[ux_html]';
-            $shortcodes .= '<div class="special-box">' . $ls_promotions_card_color . ' Deal</div>';
+            $shortcodes .= '<div class="special-box">' . $ls_promotions_short_title . '</div>';
             $shortcodes .= '<a href="' . $ls_promotions_link . '" class="clickable-card-link"></a>';
             $shortcodes .= '[/ux_html]';
 
-            // Add the promotion image if available
+            // Fetch the featured image and display if available
             if ($ls_promotions_image) {
                 $shortcodes .= '[ux_image id="' . $ls_promotions_image . '"]';
             }
-            $shortcodes .= '[row_inner_1]';
-            $shortcodes .= '[col_inner_1 span__sm="12" padding="30px 30px 0px 30px"]';
+            
+            $shortcodes .= '[row_inner]';
+            $shortcodes .= '[col_inner span__sm="12" padding="30px 30px 0px 30px"]';
             $shortcodes .= '<h4>' . $ls_promotions_title . '</h4>';
             $shortcodes .= $ls_promotions_short_description;
             $shortcodes .= '[button text="Learn More" color="alert" style="link" expand="0" icon="icon-angle-right"]';
-            $shortcodes .= '[/col_inner_1]';
-            $shortcodes .= '[/row_inner_1]';
             $shortcodes .= '[/col_inner]';
             $shortcodes .= '[/row_inner]';
             $shortcodes .= '[/col]';
@@ -71,5 +68,11 @@ function ls_shortcode_promotions()
         // Display a message if no specials are found
         $shortcodes .= '<p>No promotions available at this time. Please call us for upcoming promotions!</p>';
     }
+
+    echo do_shortcode($shortcodes);
+
+    wp_reset_postdata();
+
+    return ob_get_clean();
 }
 add_shortcode('ls_promotions', 'ls_shortcode_promotions');
--- inc/ls-shortcodes/ls-shortcode-specials.php
+++ inc/ls-shortcodes/ls-shortcode-specials.php
@@ -21,7 +21,6 @@ function ls_shortcode_specials()
     );
 
     $shortcodes = '';
-    $shortcodes .= '[row style="collapse" width="full-width" v_align="middle" h_align="center"]';
 
     $ls_shortcode_specials_args = array(
         'post_type'      => 'specials',
@@ -43,26 +42,23 @@ function ls_shortcode_specials()
             $ls_specials_title = get_field('ls_specials_title');
             $ls_specials_short_description = get_field('ls_specials_short_description');
 
-            // Build the special shortcode
-            $shortcodes .= '[col span="3" span__sm="12" span__md="10" padding="0px 0px 0px 60px" padding__md="0px 0px 0px 0px"]';
-            $shortcodes .= '[row_inner]';
-            $shortcodes .= '[col_inner span__sm="12" bg_color="rgb(255,255,255)" class="special-clickable-card ' . $ls_css_class_map[$ls_specials_what_day] . '"]';
+            // Build the promotion shortcode
+            $shortcodes .= '[col span="4" span__sm="12" bg_color="rgb(255,255,255)" animate="fadeInUp" class="special-clickable-card ' . $ls_css_class_map[$ls_specials_what_day] . '"]';
             $shortcodes .= '[ux_html]';
             $shortcodes .= '<div class="special-box">' . $ls_specials_what_day . ' Deal</div>';
             $shortcodes .= '<a href="' . $ls_special_link . '" class="clickable-card-link"></a>';
             $shortcodes .= '[/ux_html]';
 
-            // Add the special image if available
+            // Fetch the featured image and display if available
             if ($ls_special_image) {
                 $shortcodes .= '[ux_image id="' . $ls_special_image . '"]';
             }
-            $shortcodes .= '[row_inner_1]';
-            $shortcodes .= '[col_inner_1 span__sm="12" padding="30px 30px 0px 30px"]';
+
+            $shortcodes .= '[row_inner]';
+            $shortcodes .= '[col_inner span__sm="12" padding="30px 30px 0px 30px"]';
             $shortcodes .= '<h4>' . $ls_specials_title . '</h4>';
             $shortcodes .= $ls_specials_short_description;
             $shortcodes .= '[button text="Learn More" color="alert" style="link" expand="0" icon="icon-angle-right"]';
-            $shortcodes .= '[/col_inner_1]';
-            $shortcodes .= '[/row_inner_1]';
             $shortcodes .= '[/col_inner]';
             $shortcodes .= '[/row_inner]';
             $shortcodes .= '[/col]';
@@ -71,8 +67,6 @@ function ls_shortcode_specials()
         // Display a message if no specials are found
         $shortcodes .= '<p>No specials available at this time. Please call us for upcoming specials!</p>';
     }
-
-    $shortcodes .= '[/row]';
 
     echo do_shortcode($shortcodes);
 
