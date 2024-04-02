a9122378718631cb60b3400c97cd4ae14a69f230
Sergio Udave <webteam@lemonadestand.org>
2024-04-02 12:37:52 -0500

    Updating attraction card styling

--- inc/ls-shortcodes/ls-shortcode-attractions.php
+++ inc/ls-shortcodes/ls-shortcode-attractions.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Display Attractions in a card format
  */
@@ -17,64 +18,50 @@ function ls_shortcode_attractions()
 
     if ($query->have_posts()) {
 
-        // Counter to keep track of number of posts
-        $counter = 0;
-
         // Opening row shortcode
-        $shortcodes = '[row width="custom" custom_width="95%" v_align="equal"]';
+        $shortcodes = '';
+        $shortcodes .= '[row h_align="center"]';
+        $shortcodes .= '[col span__sm="12" span__md="10"]';
+        $shortcodes .= '[row_inner col_bg_radius="3" v_align="equal" padding="0px 0px 0px 0px" class="align-buttons"]';
 
-        while ( $query->have_posts() ) {
+        while ($query->have_posts()) {
 
             $query->the_post();
 
-            // Increment once for each post
-            $counter++;
-
             // Declare variables
             $ls_attraction_title             = get_the_title();
-            $ls_attraction_title             = preg_replace( '/\s+/', '<br>', $ls_attraction_title );
+            //$ls_attraction_title             = preg_replace('/\s+/', '<br>', $ls_attraction_title);
             $ls_attraction_featured_image    = get_the_post_thumbnail_url();
-            $ls_attraction_short_description = get_field( 'ls_attraction_short_description' );
-            $ls_attraction_learn_more_link   = get_field( 'ls_learn_more_button_link' );
+            $ls_attraction_short_description = get_field('ls_attraction_short_description');
+            $ls_attraction_learn_more_link   = get_field('ls_learn_more_button_link');
 
-            // Determine animation based on counter. 1-4 fadeInLeft, 5-8 fadeInRight
-            $ls_attraction_animation_class = ( ( ($counter - 1 ) % 8 ) < 4 ) ? "fadeInLeft" : "fadeInRight";
-
-            // Attractions card shortcode 
-            $shortcodes .= '[col span="3" span__sm="12" span__md="6" bg_radius="12" animate="' . $ls_attraction_animation_class .  '" depth_hover="3" class="show-radius gradient-blue-col"]';
-            $shortcodes .= '[ux_banner height="300px" bg="' . $ls_attraction_featured_image . '" bg_color="rgba(255, 255, 255, 0)" bg_overlay="rgba(0, 0, 0, 0.4)" hover="zoom"]';
-            $shortcodes .= '[text_box position_x="50" position_y="50"]';
-            $shortcodes .= '[/text_box]';
-            $shortcodes .= '[/ux_banner]';
-            $shortcodes .= '[row_inner h_align="center"]';
-            $shortcodes .= '[col_inner span="8" span__sm="12" margin="-150px 0px 0px 0px"]';
-            $shortcodes .= '[ux_banner height="250px" bg="517" bg_color="rgba(255, 255, 255, 0)"]';
-            $shortcodes .= '[text_box width="100" position_x="50" position_y="75"]';
-            $shortcodes .= '[ux_text font_size="1.3"]';
-            $shortcodes .= '<h3 class="text-yellow"><strong>' . $ls_attraction_title . '</strong></h3>';
+            $shortcodes .= '[col_inner span="4" span__sm="12" span__md="6" margin="0px 0px 50px 0px" margin__sm="0px 0px 30px 0px" margin__md="0px 0px 40px 0px" bg_color="rgb(255,255,255)" class="special-clickable-card attraction-card"]';
+            $shortcodes .= '[ux_html]';
+            $shortcodes .= '<a class="clickable-card-link" href="' . $ls_attraction_learn_more_link . '"></a>';
+            $shortcodes .= '[/ux_html]';
+            $shortcodes .= '[ux_image id="' . $ls_attraction_featured_image . '" height="75%" class="mb-0"]';
+            $shortcodes .= '[row_inner_1]';
+            $shortcodes .= '[col_inner_1 span__sm="12" padding="30px 30px 0px 30px" align="center"]';
+            $shortcodes .= '[ux_text font_size="1.4"]';
+            $shortcodes .= '<h4>' . $ls_attraction_title . '</h4>';
             $shortcodes .= '[/ux_text]';
-            $shortcodes .= '[/text_box]';
-            $shortcodes .= '[/ux_banner]';
-            $shortcodes .= '[/col_inner]';
-            $shortcodes .= '[col_inner span="10" span__sm="12" margin="-29px 0px 0px 0px" align="center" color="light"]';
+            $shortcodes .= '[ux_text font_size="0.9"]';
             $shortcodes .= '<p>' . $ls_attraction_short_description . '</p>';
-            $shortcodes .= '[button text="Learn More" style="link" size="large" class="text-yellow" link="' . $ls_attraction_learn_more_link . '"]';
+            $shortcodes .= '[/ux_text]';
+            $shortcodes .= '[/col_inner_1]';
+            $shortcodes .= '[/row_inner_1]';
+            $shortcodes .= '[button text="Learn More" color="alert" style="outline" padding="5px 30px 5px 30px" expand="0" icon="icon-angle-right" class="attraction-button"]';
             $shortcodes .= '[/col_inner]';
-            $shortcodes .= '[/row_inner]';
-            $shortcodes .= '[/col]';
-
-            // After every 4th post, close the current row and start a new one or else the animation will be off
-            if ( $counter % 4 == 0 && $query->current_post + 1 < $query->post_count ) {
-                // Close the current row and start a new one
-                $shortcodes .= '[/row][row width="custom" custom_width="95%" v_align="equal"]';
-            }
         }
 
-        // Close off last row
+        // Close off opening shortcodes
+        $shortcodes .= '[/row_inner]';
+        $shortcodes .= '[/col]';
         $shortcodes .= '[/row]';
 
+
         // Output the shortcodes
-        echo do_shortcode( $shortcodes );
+        echo do_shortcode($shortcodes);
 
         // Clean up
         wp_reset_postdata();
